45ac7d6da2fb7f210d879a8e696e63143b444738
ND <nathanield14@gmail.com>
2025-11-06 22:04:56 -0700

    Adding delta and FPS fixes

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_end = True
     while game_end:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill(("Black"))
         pygame.display.flip()
+        ticking = clock.tick(60)
+        dt = ticking / 1000
 
 
 if __name__ == "__main__":
