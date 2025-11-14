d5402ef3a8823798090e6ff5a9741b040af74b14
ND <nathanield14@gmail.com>
2025-11-13 21:33:26 -0700

    adding collision

--- circleshape.py
+++ circleshape.py
@@ -20,4 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
-        
+
+    def collides_with(self, other):
+        collided = False
+        distance = self.position.distance_to(other.position)
+        if distance <= self.radius + other.radius:
+            collided = True
+        return collided
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -9,11 +10,13 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -26,16 +29,27 @@ def main():
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     game_end = True
+
     while game_end:
+
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(("Black"))
 
         for item in drawable:
             item.draw(screen)
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
 
         pygame.display.flip()
         ticking = clock.tick(60)
